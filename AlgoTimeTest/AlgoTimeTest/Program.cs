cdc533c965bc12af37592b1a71eee89fb8e77923
nodlya <nadya-popova-02@mail.ru>
2021-09-28 00:26:40 +0500

    6-8 algos

--- AlgoTimeTest/AlgoTimeTest/Program.cs
+++ AlgoTimeTest/AlgoTimeTest/Program.cs
@@ -9,7 +9,8 @@ namespace AlgoTimeTest
     {
         public static Random Rnd = new Random(666);
         public static string Path = @"Data.csv";
-        public static List<int> ElapsedTime = new List<int>(); 
+        public static List<int> ElapsedTime = new List<int>();
+        
         static void Main(string[] args)
         {
             int times = int.Parse(Console.ReadLine());
@@ -52,15 +53,15 @@ namespace AlgoTimeTest
             //for (int i = 0; i < k; i++)
             //    mas[i] = Rnd.Next();
 
-            int[,] mas1 = new int[k,k];
-            int[,] mas2 = new int[k, k];
-            for (int i = 0;i<k;i++)
-                for (int j = 0; j<k;j++)
-                {
-                    mas1[i, j] = Rnd.Next();
-                    mas2[i, j] = Rnd.Next();
-                }
-            int[,] result = new int[mas1.Length, mas1.Length];
+            //int[,] mas1 = new int[k,k];
+            //int[,] mas2 = new int[k, k];
+            //for (int i = 0;i<k;i++)
+            //    for (int j = 0; j<k;j++)
+            //    {
+            //        mas1[i, j] = Rnd.Next();
+            //        mas2[i, j] = Rnd.Next();
+            //    }
+            //int[,] result = new int[mas1.Length, mas1.Length];
 
             //int sum=0;
             //int multiple = 1;
@@ -69,6 +70,10 @@ namespace AlgoTimeTest
             //var x = 1.5;
             //double p = 0;
 
+            int x = Rnd.Next(1000);
+            int c = x;
+            int n = k;
+            int result=1;
             stopwatch.Start();
 
             //start algo
@@ -97,23 +102,189 @@ namespace AlgoTimeTest
             //        }
 
             //6 algo
-            
-            for (int i = 0; i < mas1.GetLength(0); i++)
+            //QuickSort(mas, 0, k-1);
+
+            //7 algo
+            //TimSort(mas,mas.Length);
+
+            //8 algo
+            while (n !=0)
             {
-                for (int j = 0; j < mas2.GetLength(1); j++)
+                if (n%2==0)
                 {
-                    for (int l = 0; k < mas1.GetLength(0); l++)
-                    {
-                        result[i, j] += result[i, l] * result[l, j];
-                    }
+                    c *= c;
+                    n /= 2;
+                }
+                else
+                {
+                    result *= c;
+                    n--;
                 }
             }
+
+
+            //9 algo
+            //for (int i = 0; i < mas1.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < mas2.GetLength(1); j++)
+            //    {
+            //        for (int l = 0; k < mas1.GetLength(0); l++)
+            //        {
+            //            result[i, j] += result[i, l] * result[l, j];
+            //        }
+            //    }
+            //}
 
             //finish algo
 
 
             stopwatch.Stop();
+
+            Console.WriteLine(x+ " "+ k +" "+ result);
             return (int)stopwatch.ElapsedTicks * 10;
+        }
+
+        static void QuickSort(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int pivot = Partition(array, start, end);
+            QuickSort(array, start, pivot - 1);
+            QuickSort(array, pivot + 1, end);
+        } 
+
+        static int Partition(int[] array, int start, int end)
+        {
+            int temp;//swap helper
+            int marker = start;//divides left and right subarrays
+            for (int i = start; i < end; i++)
+            {
+                if (array[i] < array[end]) //array[end] is pivot
+                {
+                    temp = array[marker]; // swap
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            //put pivot(array[end]) between left and right subarrays
+            temp = array[marker];
+            array[marker] = array[end];
+            array[end] = temp;
+            return marker;
+        }
+
+
+        // Iterative Timsort function to sort the
+        // array[0...n-1] (similar to merge sort)
+        public static void TimSort(int[] arr, int n)
+        {
+            int RUN = 32;
+            // Sort individual subarrays of size RUN
+            for (int i = 0; i < n; i += RUN)
+                InsertionSort(arr, i,
+                             Math.Min((i + RUN - 1), (n - 1)));
+
+            // Start merging from size RUN (or 32).
+            // It will merge
+            // to form size 64, then
+            // 128, 256 and so on ....
+            for (int size = RUN; size < n;
+                                     size = 2 * size)
+            {
+
+                // Pick starting point of
+                // left sub array. We
+                // are going to merge
+                // arr[left..left+size-1]
+                // and arr[left+size, left+2*size-1]
+                // After every merge, we increase
+                // left by 2*size
+                for (int left = 0; left < n;
+                                      left += 2 * size)
+                {
+
+                    // Find ending point of left sub array
+                    // mid+1 is starting point of
+                    // right sub array
+                    int mid = left + size - 1;
+                    int right = Math.Min((left +
+                                        2 * size - 1), (n - 1));
+
+                    // Merge sub array arr[left.....mid] &
+                    // arr[mid+1....right]
+                    if (mid < right)
+                        Merge(arr, left, mid, right);
+                }
+            }
+        }
+
+        public static void InsertionSort(int[] arr, int left, int right)
+        {
+            for (int i = left + 1; i <= right; i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+                while (j >= left && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+        public static void Merge(int[] arr, int l, int m, int r)
+        {
+            // original array is broken in two parts
+            // left and right array
+            int len1 = m - l + 1, len2 = r - m;
+            int[] left = new int[len1];
+            int[] right = new int[len2];
+            for (int x = 0; x < len1; x++)
+                left[x] = arr[l + x];
+            for (int x = 0; x < len2; x++)
+                right[x] = arr[m + 1 + x];
+
+            int i = 0;
+            int j = 0;
+            int k = l;
+
+            // After comparing, we merge those two array
+            // in larger sub array
+            while (i < len1 && j < len2)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements
+            // of left, if any
+            while (i < len1)
+            {
+                arr[k] = left[i];
+                k++;
+                i++;
+            }
+
+            // Copy remaining element
+            // of right, if any
+            while (j < len2)
+            {
+                arr[k] = right[j];
+                k++;
+                j++;
+            }
         }
 
         static void EmptyCsv()
